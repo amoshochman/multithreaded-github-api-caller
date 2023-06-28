b16279d01e4d2e29c1b6822e862bab020958cbee
amoshochman <amoshochman1982@gmail.com>
2023-06-28 18:44:32 +0300

    initial commit

---
+++ config-example.py
@@ -0,0 +1,5 @@
+TOKEN = "<your token>"
+QUERIES_FILE = "<path to txt with git api calls>"
+MAX_THREADS_NUM = 4
+LOG_FILE = "<path to log file>"
+TIME_OUT = 3
---
+++ main.py
@@ -0,0 +1,61 @@
+import logging
+import sys
+from threading import Thread
+
+import requests
+import config
+
+logging.basicConfig(filename=config.LOG_FILE, level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+# pool, queue, retry, maybe try except in log, CHECK IF RATELIMIT HAS REACHED... IN THIS CASE WRITE THE TIME AND NOT TRY ANYMORE, classes
+#
+
+def call_git_api(url, results):
+    headers = {
+        'Authorization': 'Bearer ' + config.TOKEN,
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    response = None
+    try:
+        response = requests.get(url, headers=headers)
+    except requests.exceptions.RequestException as e:
+        logging.error(f"{e} - Query: {url}")
+    if not response:
+        logging.error(f"API request failed with status code {response.status_code} - Query: {url}")
+        logging.error(f"Response body: {response.text}")
+        return
+    if response.status_code == 200:
+        try:
+            count = response.json().get('total_count')
+            results.append(count)
+        except (ValueError, KeyError):
+            logging.error(f"No total_count on response - Query: {url}")
+
+
+
+
+def parse_files(path):
+    results = []
+    threads = []
+    try:
+        with open(path, encoding='utf-8') as file:
+            queries = [query.strip() for query in file]
+    except OSError as e:
+        logging.error(f"An error occurred while trying to read file '{path}': {str(e)}")
+        sys.exit(1)
+    for query in queries:
+        while config.MAX_THREADS_NUM <= len([thread for thread in threads if thread.is_alive()]):
+            pass
+        thread = Thread(target=call_git_api, args=(query.strip(), results))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join(timeout=config.TIME_OUT)
+        if thread.is_alive():
+            logging.error(f"A thread took longer than the specified timeout: {thread}")
+    print(results)
+
+
+if __name__ == '__main__':
+    parse_files(config.QUERIES_FILE)
